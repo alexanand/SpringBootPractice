a4dc0f1c08338057bb9ae0eed3ff5ad32ddcc9d1
alexanand <anandkarkumar5@gmail.com>
2022-07-28 01:43:34 +0530

    All Mapping Done

--- ZuulPractice/src/main/java/com/practise/zuul/ZuulPractice/Testcontroller.java
+++ ZuulPractice/src/main/java/com/practise/zuul/ZuulPractice/Testcontroller.java
@@ -1,13 +1,45 @@
 package com.practise.zuul.ZuulPractice;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class Testcontroller {
 	
+//	@RequestMapping("/")
+//	public String index() {
+//		
+//		return "index";
+//	}
 	
 	
+
+	@RequestMapping("/Api")
+	public String home() {
+		
+		return "Registration";
+	}
+	
+	@RequestMapping("/regform")
+	public String register(Model m, String data, HttpServletRequest req) {
+		
+		m.addAttribute("email", req.getParameter("email"));
+		
+		return "home";
+	}
+	
+//	@RequestMapping("/error")
+//	public String error() {
+//		
+//		return "error";
+//	}
 
 }
--- ZuulPractice/src/main/java/com/practise/zuul/ZuulPractice/ZuulPracticeApplication.java
+++ ZuulPractice/src/main/java/com/practise/zuul/ZuulPractice/ZuulPracticeApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 
 /*@EnableZuulProxy  
 @EnableDiscoveryClient  */
 
 @SpringBootApplication
 @EnableScheduling
+//@EnableAdminServer
 public class ZuulPracticeApplication {
 
 
